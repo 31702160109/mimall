d1ca247c69a07506cfcd7de741dfc53346011124
xin <cxbhqq@163.com>
2020-03-07 23:52:38 +0800

    Nav-Header组件修复背景色

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -204,6 +204,7 @@ export default {
           opacity: 0;
           overflow: hidden;
           border-top: 1px solid #E5E5E5;
+          background-color: #FFFFFF;
           box-shadow:0px 7px 6px 0px rgba(0, 0, 0, 0.11);
           transition: all .3s;
           .product {
